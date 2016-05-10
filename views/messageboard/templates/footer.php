c0aa9cbe06b08d8386236fe5f9091c64c54adb83
A-N-Other92 <A-N-Other92@users.noreply.github.com>
2016-05-10 22:17:13 +0100

    added link to code listing
    Added link to the code listing on github

--- views/messageboard/templates/footer.php
+++ views/messageboard/templates/footer.php
@@ -1,7 +1,10 @@
 <div class="row row-red">
-        <div class="col-md-4 no-padding">Message board</div>
-        <div class="col-md-4 no-padding">By Lee</div>
-        <div class="col-md-4 no-padding">2015</div>
+        <div class="col-md-4 no-padding">&nbsp;&nbsp;Message board</div>
+        <div class="col-md-4 no-padding"></div>
+        <div class="col-md-1 no-padding">By Lee</div>
+        <div class="col-md-1 no-padding">2015</div>
+        <div class="col-md-2 no-padding"><a class="underline" href="https://github.com/A-N-Other92/messageboard_php_codeigniter" target="_blank"><button type="button" class="btn btn-danger btn-sm btn-block button-text-black"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> View source code</button></a></div>
+
       </div>
  
 </div>  <!-- end of fluid container -->
